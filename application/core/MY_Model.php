a4e94cb770b5092fb2f89f72db186f5c7751dd6c
Justin Favaloro <justin@justin.com>
2013-09-24 21:59:07 +1000

    Added useInlinedReferences extension option for the api calls

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -2,7 +2,6 @@
 
 class MY_Model extends CI_Model {
 
-    public $appInfo = '';
     public $type = '';
 
     function __construct($type = null) {
@@ -11,14 +10,27 @@ class MY_Model extends CI_Model {
       $this->initialize();
     }
 
+    /**
+     * Initialize the base url for the RESTful API
+     * @return [type]
+     */
     function initialize() {
       $this->rest->initialize(array(
         'server' => 'https://api.flightstats.com/flex/'.$this->type.'/rest/v1/json/'
       ));
-      $this->appInfo = array(
+    }
+
+    /**
+     * Make a request to the RESTful api
+     * @param  [string] $function - the REST function that needs to be called
+     * @return [object] A collection of information related to the $function
+     */
+    function apiCall($function) {
+      return $this->rest->get($function, array(
         'appId' => 'bf28d4a0',
-        'appKey' => 'a7ca1c2a0eab46e9d097f4aa39168ac7'
-      );
+        'appKey' => 'a7ca1c2a0eab46e9d097f4aa39168ac7',
+        'extendedOptions' => 'useInlinedReferences'
+      ));
     }
 }
 
