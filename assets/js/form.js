ceefe86d7dbefd19fcd21aa7c487ae1ad191262d
ddksaku <ddksaku@gmail.com>
2013-10-07 09:53:38 +0800

    Added new project asset files

--- assets/js/form.js
+++ assets/js/form.js
@@ -6,3 +6,65 @@ $(document).ready(function() {
     autoclose: 'true'
   });
 });
+
+
+
+/**
+ * Added by David Ming 2013/10/5
+ * Enable autocomplete of Airport Code field
+ * @param suggest_url
+ */
+
+function enable_search_airport(suggest_url)
+{
+	$('#arrivalAirportCode').click(function() {
+		$(this).val('');
+    });
+
+	$("#arrivalAirportCode").autocomplete({minLength: 1,
+	  	source: function( request, response ) {
+	  		$.ajax({
+	  			type : "POST" ,
+	  			url:  suggest_url,
+	  			dataType: "json" ,
+	  			data: {term:request.term} ,
+	  			error : function(request, status, error) {
+	  		         alert("code");
+	  		        },
+	  			success: function(data) {
+	  				response(data);
+	  			}
+	  		});
+	  	}
+	  });
+}
+
+/**
+ * Added by David Ming 2013/10/5
+ * Enable autocomplete of Carrier Code
+ * @param suggest_url
+ */
+
+function enable_search_flight(suggest_url)
+{
+	$('#carrierCode').click(function() {
+		$(this).val('');
+    });
+
+	$("#carrierCode").autocomplete({minLength: 1,
+	  	source: function( request, response ) {
+	  		$.ajax({
+	  			type : "POST" ,
+	  			url:  suggest_url,
+	  			dataType: "json" ,
+	  			data: {term:request.term} ,
+	  			error : function(request, status, error) {
+	  		         alert("code");
+	  		        },
+	  			success: function(data) {
+	  				response(data);
+	  			}
+	  		});
+	  	}
+	  });
+}
