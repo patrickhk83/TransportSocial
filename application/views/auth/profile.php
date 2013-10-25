3dc2eb97e75135445298589a2d2ea01aa4c0e59f
justinf111 <justin_f_111@hotmail.com>
2013-10-25 19:23:57 +1100

    Refactored the profile page
    Remove unnecessary event listeners, rename user_data to user, and made
    the photo section loop over all photos and display each instead of
    always showing 5

--- application/views/auth/profile.php
+++ application/views/auth/profile.php
@@ -1,59 +1,43 @@
 <script type="text/javascript">
 	$(document).ready(function()
 	{
-		//$("#pppp").fancybox();
-
-		enable_user_dialog("<?php echo site_url("$controller_name/get_user_dialog_info");?>" , "<?php echo base_url("assets/images/default-profile-pic.png");?>" , <?php echo $user_data->id;?>);
-		document.getElementById('files1').addEventListener('change' , handleFileSelect1 , false);
-		document.getElementById('files2').addEventListener('change' , handleFileSelect2 , false);
-		document.getElementById('files3').addEventListener('change' , handleFileSelect3 , false);
-		document.getElementById('files4').addEventListener('change' , handleFileSelect4 , false);
-		document.getElementById('files5').addEventListener('change' , handleFileSelect5 , false);
+		enable_user_dialog("<?php echo site_url($this->router->fetch_class().'/edit_user/'.$user->id);?>" , "<?php echo base_url("assets/images/default-profile-pic.png");?>" , <?php echo $user->id;?>);
 	});
-
 </script>
-<h1 class="heading"><?php echo $user_data->first_name." ".$user_data->last_name;?></h1>
-<input type="button" class="btn btn-primary" value="Edit profile" id="signup_btn" name="signup_btn" data-toggle="modal">
-<div class="photo"><img id="pppp" class="thumb200" src="<?php echo $default_character;?>" /></div>
+<h1 class="heading"><?php echo $user->first_name." ".$user->last_name;?></h1>
+<a href="/auth/edit_user/<?php echo $user->id; ?>" class="btn btn-primary" data-toggle="modal" data-target="#dialog_form" data-remote="false">Edit Profile</a>
+<div class="photo"><img class="thumb" src="<?php echo $profile_pic;?>" /></div>
 <div class="occupation">
 	<p>
-		<?php echo $user_data->company. '|' .$country;?>
+		<?php echo $user->company. '|' .$country;?>
 	</p>
 </div>
-
-<?php if(!empty($user_data->about_me)) ?>
+<?php if(!empty($user->about_me)): ?>
 	<h1 class="heading"><?php echo lang('my_profile_about_me');?></h1>
-	<p class="content"><?php echo $user_data->about_me;?></p>
+	<p class="content"><?php echo $user->about_me;?></p>
 <?php endif; ?>
-<?php if(!empty($user_data->hobbies)): ?>
+<?php if(!empty($user->hobbies)): ?>
 		<h1 class="heading"><?php echo lang('my_profile_hobbies');?></h1>
-		<p class="content"><?php echo $user_data->hobbies;?></p>
+		<p class="content"><?php echo $user->hobbies;?></p>
 <?php endif; ?>
-<?php if(!empty($user_data->musics)): ?>
+<?php if(!empty($user->musics)): ?>
 		<h1 class="heading"><?php echo lang('my_profile_musics');?></h1>
-		<p class="content"><?php echo $user_data->musics;?></p>
+		<p class="content"><?php echo $user->musics;?></p>
 <?php endif; ?>
-<?php if(!empty($user_data->movies)): ?>
+<?php if(!empty($user->movies)): ?>
 		<h1 class="heading"><?php echo lang('my_profile_movies');?></h1>
-		<p class="content"><?php echo $user_data->movies;?></p>
+		<p class="content"><?php echo $user->movies;?></p>
 	<?php endif; ?>
-<?php if(!empty($user_data->books)): ?>
+<?php if(!empty($user->books)): ?>
 		<h1 class="heading"><?php echo lang('my_profile_books');?></h1>
-		<p class="content"><?php echo $user_data->books;?></p>
+		<p class="content"><?php echo $user->books;?></p>
 <?php endif; ?>
 
+<?php if (count($photos)): ?>
 <h1 class="heading"><?php echo lang('my_profile_my_photos'); ?></h1>
-<div>
-	<img class="thumb200" src="<?php echo $photo1; ?>" />
-	<img class="thumb200" src="<?php echo $photo2; ?>" />
-	<img class="thumb200" src="<?php echo $photo3; ?>" />
-	<img class="thumb200" src="<?php echo $photo4; ?>" />
-	<img class="thumb200" src="<?php echo $photo5; ?>" />
-</div>
-
-<?php
-	$data['countries'] = $countries;
-	$data['controller_name'] = $controller_name;
-	$this->load->view("auth/create_user" , $data);
-
-?>
+	<div>
+		<?php foreach ($photos as $photo): ?>
+			<img class="thumb" src="<?php echo $photo->path; ?>" />
+		<?php endforeach; ?>
+	</div>
+<?php endif ?>
